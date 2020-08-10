d2c7267c9610c5ad202005abb575070a1b0ca4d6
Marko <59150246+MarkoNikolicViser@users.noreply.github.com>
2020-08-10 10:28:16 +0200

    Add files via upload

---
+++ precache-manifest.b303053a9e5358eb26bfe766c8205f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2753b370f736bd37cc86bda106a7663a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e258bd315aa7002356d3",
+    "url": "/static/css/main.6f1d040e.chunk.css"
+  },
+  {
+    "revision": "11032487abcf409cd9c2",
+    "url": "/static/js/2.c768ad4c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.c768ad4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e258bd315aa7002356d3",
+    "url": "/static/js/main.5a27a5ee.chunk.js"
+  },
+  {
+    "revision": "abd8b9d6a9bafb4ba6ce",
+    "url": "/static/js/runtime-main.bf1f4d57.js"
+  }
+]);
